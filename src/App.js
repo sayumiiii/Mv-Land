3cb1383b4dbbde17971d06cf4c2bd9fc1a531e7e
sayumiiii <sayumihasanjali@icloud.com>
2023-02-19 17:27:46 -0500

    v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
+
 
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
@@ -14,18 +14,19 @@ const App = () => {
     searchMovies("Batman");
   }, []);
 
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
   };
-
   return (
     <div className="app">
-      <h1>MovieLand</h1>
 
-      <div className="search">
+      <h1>MovieSearch</h1>
+      <div></div>
+     <div className="search">
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
